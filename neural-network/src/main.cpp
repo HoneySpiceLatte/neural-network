608afa35e54412439285c11397c3d1fb312761e6
Latte <clearloadsave@gmail.com>
2019-12-06 01:08:47 -0800

    Setup thread

--- neural-network/src/main.cpp
+++ neural-network/src/main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
+
+static bool isRunning = false;
+
+void init() {
+	isRunning = true;
+}
+
+void input() {
+
+}
+
+void update() {
+
+}
+
+void draw() {
+
+}
 
 int main() {
 
+	init();
+
+	while (isRunning) {
+
+		// Game logic
+		input();
+		update();
+		draw();
+
+		std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(1));
+	}
 }
