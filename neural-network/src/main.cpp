03c308504c4ca38a16e94770e7ac64134a40f18a
Latte <clearloadsave@gmail.com>
2020-02-22 23:11:18 -0800

    Game Loop Delay

--- neural-network/src/main.cpp
+++ neural-network/src/main.cpp
@@ -2,9 +2,15 @@
 #include <chrono>
 #include <thread>
 
-using namespace std;
-
 static bool isRunning = false;
+
+const int FPS = 5;
+
+// Macros
+template <typename T>
+auto seconds_to_duration(T seconds) {
+	return std::chrono::duration<T, std::ratio<1>>(seconds);
+}
 
 void init() {
 	isRunning = true;
@@ -18,8 +24,8 @@ void update() {
 
 }
 
-void draw() {
-
+void render() {
+	std::cout << "Cyka" << std::endl;
 }
 
 int main() {
@@ -28,11 +34,15 @@ int main() {
 
 	while (isRunning) {
 
-		// Game logic
+		auto start = std::chrono::system_clock::now();
+
 		input();
 		update();
-		draw();
+		render();
 
-		this_thread::sleep_until(chrono::system_clock::now() + chrono::seconds(1));
+		std::chrono::duration<double> elapsed = std::chrono::system_clock::now() - start;
+		double delay = ( 1000 / FPS ) - std::chrono::duration<double>( elapsed ).count();
+
+		std::this_thread::sleep_for( std::chrono::milliseconds( (long long)delay ) );
 	}
 }
