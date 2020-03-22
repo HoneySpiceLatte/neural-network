690fc5b565d61786d17a1f1533e25f903980a021
Latte <clearloadsave@gmail.com>
2020-03-22 12:47:44 -0700

    Test

--- neural-network/src/main.cpp
+++ neural-network/src/main.cpp
@@ -1,32 +1,43 @@
-#include <iostream>
-
 #include <SDL.h>
+
+#include <iostream>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
 
-int main(int argc, char* argv[]) {
-	
-	if ( SDL_Init(SDL_INIT_EVERYTHING) < 0 )
-		std::cout << "SDL failed to initialize! SDL Error: " << SDL_GetError() << std::endl;
+int main(int argc, char* argv[])
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+        std::cout << "SDL failed to initialize: " << SDL_GetError() << std::endl;
 
-	SDL_Window* window = SDL_CreateWindow(
-		"Application", // Window Title
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, // Window Pos
-		WIDTH, HEIGHT, // Window Size
-		SDL_WINDOW_ALLOW_HIGHDPI // Flags
-	);
+    SDL_Window* window = SDL_CreateWindow("Application", // Window Title
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, // Window Pos
+        WIDTH,
+        HEIGHT, // Window Size
+        SDL_WINDOW_ALLOW_HIGHDPI // Flags
+    );
 
-	if (window == NULL)
-		std::cout << "Window failed to create: " << SDL_GetError() << std::endl;
+    if (window == NULL) {
+        std::cout << "Window failed to create: " << SDL_GetError() << std::endl;
+        return 1;
+    }
 
-	SDL_Event windowEvent;
+    SDL_Event windowEvent;
 
-	while (true) {
-		if ( SDL_PollEvent(&windowEvent) ) {
+    while (true) {
+        if (SDL_PollEvent(&windowEvent)) {
+            switch (windowEvent.type) {
+            case SDL_QUIT:
+                goto break_loop;
+            }
+        }
+    }
 
-		}
-	}
+break_loop:
 
-	return EXIT_SUCCESS;
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
